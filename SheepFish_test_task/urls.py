cc452ddd56f907b28ef8baf720aea81e0c344a8d
phaishuk <xomas@ukr.net>
2023-09-02 20:46:04 +0300

    create app check_handling_service, add it to settings, create urls file in this app

--- SheepFish_test_task/urls.py
+++ SheepFish_test_task/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "api/check_handling_service/",
+        include("check_handling_service.urls", namespace="check_handling_service"),
+    ),
 ]
---
+++ check_handling_service/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework import routers
+
+# router = routers.DefaultRouter()
+# router.register("posts", MyModel, basename="post")
+#
+# urlpatterns = [
+#     path("", include(router.urls)),
+# ]
+#
+# app_name = "check_handling_service"
