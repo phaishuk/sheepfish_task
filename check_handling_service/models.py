a522986f4ae86a10420328e785f539506b880147
phaishuk <xomas@ukr.net>
2023-09-08 10:33:04 +0300

    create models

--- check_handling_service/models.py
+++ check_handling_service/models.py
@@ -24,8 +24,13 @@ class Check(models.Model):
         ("printed", "Printed"),
     ]
 
-    printer_id = models.ForeignKey(to=Printer, on_delete=models.CASCADE)
+    printer_id = models.ForeignKey(
+        to=Printer, on_delete=models.CASCADE, related_name="checks"
+    )
     type = models.CharField(max_length=7, choices=CHECK_TYPE_CHOICES)
     order = models.JSONField()
     status = models.CharField(max_length=8, choices=CHECK_STATUS_CHOICES)
     pdf_file = models.FileField(null=True, blank=True)
+
+    def __str__(self):
+        return self.order.order_number
