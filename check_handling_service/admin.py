13bf8eff5ac08f642d5c99ae44d0196d199da62f
phaishuk <xomas@ukr.net>
2023-09-08 10:33:18 +0300

    create create admin panel

--- check_handling_service/admin.py
+++ check_handling_service/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
-
 from check_handling_service.models import Printer, Check
 
-admin.site.register(Printer)
-admin.site.register(Check)
+
+class CheckAdmin(admin.ModelAdmin):
+    list_display = ("order_number", "point_id", "printer_id", "type", "status")
+    list_filter = ("printer_id", "type", "status")
+    search_fields = ("printer_id__name",)
+    readonly_fields = ("order_number", "point_id")
+
+    def order_number(self, obj):
+        return obj.order.get("order_number")
+
+    def point_id(self, obj):
+        return obj.order.get("point_id")
+
+
+class PrinterAdmin(admin.ModelAdmin):
+    list_display = ("name", "api_key", "check_type", "point_id")
+    search_fields = ("name", "api_key")
+
+
+admin.site.register(Printer, PrinterAdmin)
+admin.site.register(Check, CheckAdmin)
